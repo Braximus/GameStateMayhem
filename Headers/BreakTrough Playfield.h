2df4afa13f15e7689391d311144dbb54c71a8543
Braximus <utopija89@gmail.com>
2016-09-16 18:26:56 +0200

    Update and rename BreakTrough Playfield.h to Headers/BreakTrough Playfield.h

--- Headers/BreakTrough Playfield.h
+++ Headers/BreakTrough Playfield.h
@@ -9,9 +9,6 @@ class BrkTr_Playfield : public BrkTr_Game_Objects
 	const sf::Texture&		m_Texture;
 	sf::RectangleShape		m_Playfield;
 	sf::VertexArray			mOutline_Vertices;
-
-
-	//	Mozda ih prebacim u zasebnu klasu!
 	sf::VertexArray			mBackground_Vertices;
 public:
 	BrkTr_Playfield(BreakTrough_Data&);
@@ -19,8 +16,4 @@ public:
 	void	draw(sf::RenderTarget& target, sf::RenderStates state) const;
 	void	update(sf::Time& time);
 };
-
-
-
-
 #endif
