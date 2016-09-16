7f9fde1d642bddb310fe0473963652893c987455
Braximus <utopija89@gmail.com>
2016-09-16 15:04:18 +0200

    Update BreakTrough Pause.h

--- BreakTrough Pause.h
+++ BreakTrough Pause.h
@@ -8,20 +8,16 @@ class BreakTrough_Data;
 
 class BreakTrough_Pause: public BrkTr_UI_Objects
 {
-	Break::Button_Selection							mCurrentSelection, mPreviousSelection;
+	Break::Button_Selection					mCurrentSelection, mPreviousSelection;
 	std::map<Break::Button_Selection, Button>		mButtons;
-	sf::RectangleShape		mPause_Area, mScreen_Fill;
-	bool					hover;
+	sf::RectangleShape					mPause_Area, mScreen_Fill;
+	bool							hover;
 public:
 	BreakTrough_Pause(BreakTrough_Data&);
 	void		draw(sf::RenderTarget& target, sf::RenderStates state) const;
 	void		interact(sf::Vector2f param);
 	const bool	is_hovering() const;
 	const Break::Button_Selection		response() const;
-
-	
 };
-
-
 
 #endif
