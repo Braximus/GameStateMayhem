c6a727a284350d1a6f4134fca81ed92050f9bee7
Braximus <utopija89@gmail.com>
2016-09-16 14:50:44 +0200

    Update and rename Comet Manager.h to Headers/Comet Manager.h

--- Headers/Comet Manager.h
+++ Headers/Comet Manager.h
@@ -7,26 +7,22 @@ class CometWarsData;
 
 class Comet_Manager
 {
-	CometWarsData&			mData;
-	std::deque<CometWars_Game_Asteroid>		mComets;	
+	CometWarsData&				mData;
+	std::deque<CometWars_Game_Asteroid>	mComets;	
 public:
 	Comet_Manager(CometWarsData&);	
 	std::deque<CometWars_Game_Objects*>		pass_pointers();
 	void			update(sf::Time&);
 	void			restart();
 
-	void							create_comet(bool);
-	void							delete_comet();
+	void					create_comet(bool);
+	void					delete_comet();
 	const CW::AsteroidDamage		damage_comet(UINT);
 	
-	void						setComet_Position(UINT, sf::Vector2f);
+	void					setComet_Position(UINT, sf::Vector2f);
 	const sf::Vector2f			getComet_Position(UINT) const;
 	const sf::FloatRect			getComet_Bounds(UINT) const;
-	const bool					isMini_Comet(UINT) const;
-	const UINT					getNumber_of_comets() const;
+	const bool				isMini_Comet(UINT) const;
+	const UINT				getNumber_of_comets() const;
 };
-
-
-
-
 #endif
