8037d09b811c8d37ce10c7f5411f9334b8b5c023
Braximus <utopija89@gmail.com>
2016-09-16 14:29:30 +0200

    Update User.h

--- Headers/User.h
+++ Headers/User.h
@@ -10,14 +10,14 @@ class Data;
 class User
 {
 	State*				ptrStateUser;
-	sf::RenderWindow&	mWindow;
+	sf::RenderWindow&		mWindow;
 	Data&				mData;
 
-	bool				Malukah;
+	bool				Screenshot;
 public:
 	User(sf::RenderWindow&, Data&);
 
-	void				passPtr(State*);	// Mora da postoji odvojena funkcija za premestanje pokazivaca!
+	void				passPtr(State*);
 
 	void				handleEvents(sf::Event&);
 	void				handleRealTimeInputs();
@@ -25,14 +25,5 @@ public:
 	void				GlobalActions(sf::Event&);
 
 };
-
-//
-// Treba da se:
-// ---	naprave akcije u skladu sa etapama igre!
-// ---	povezu akcije sa odredjenim tasterima!
-// ---	da mogucnost da se tasteri promene!
-// ---	da program prepozna kada koja akcija da se preduzme u zavisnosti od trenutne etape igre!
-//
-
 
 #endif
