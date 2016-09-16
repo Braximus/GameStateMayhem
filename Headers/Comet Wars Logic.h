e4fe22ed24f46ffc84e78905b3239bfbfdde4f87
Braximus <utopija89@gmail.com>
2016-09-16 14:59:34 +0200

    Update and rename Comet Wars Logic.h to Headers/Comet Wars Logic.h

--- Headers/Comet Wars Logic.h
+++ Headers/Comet Wars Logic.h
@@ -11,17 +11,15 @@ class Comet_Manager;
 
 class CometWarsLogic
 {
-
 	CometWarsData&			mData;
 	CometWars_UI_Manager*	pUI_Manager;
 	CometWars_Player*		pPlayer;
 	Comet_Manager*			pComet_Manager;
-	//	Sudari:
+	//	Collisions:
 	void				Collision_Object_Border_Check();
 	void				Collision_Player_Check();
-
-	//	Operacije:
-	void				Calculate_position(CW::Zones zone, sf::Vector2f& position);	//	Glupo ime, da promenim u logicnije, ali da bude sazeto.
+	//	Operations:
+	void				Calculate_position(CW::Zones zone, sf::Vector2f& position);
 	void				Asteroid_Ship_Response();
 	void				Asteroid_Damage_Response(UINT param);
 	void				Big_Comet_Destroyed_Response(UINT param);
@@ -30,9 +28,5 @@ class CometWarsLogic
 public:
 	explicit			CometWarsLogic(CometWarsData& data, CometWars_UI_Manager* pUI, CometWars_Player* pPlay, Comet_Manager* pComets);
 	void				LogicFunction();
-
 };
-
-
-
 #endif
