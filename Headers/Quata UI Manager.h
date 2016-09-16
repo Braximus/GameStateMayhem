3f019c7adb21096c0e7093c515c5e2b9f9d7119d
Braximus <utopija89@gmail.com>
2016-09-16 15:00:46 +0200

    Update and rename Quata UI Manager.h to Headers/Quata UI Manager.h

--- Headers/Quata UI Manager.h
+++ Headers/Quata UI Manager.h
@@ -8,30 +8,25 @@ class QuataData;
 
 class Quata_UI_Manager
 {
-	QuataData&		mData;
-	std::map<Qua::Quata_Texts, Quata_UI_Text>				mTexts;	//	 Oznake i skorovi!
+	QuataData&							mData;
+	std::map<Qua::Quata_Texts, Quata_UI_Text>			mTexts;
 	std::map<Qua::Quata_UI_Components, Quata_UI_Text>		mScores;
 	std::unique_ptr<Quata_Pause>					pPause;
-	std::unique_ptr<Quata_SplashScreen>					pSplashScreen;
+	std::unique_ptr<Quata_SplashScreen>				pSplashScreen;
 
 	bool			is_pause_on, is_SplashScreen_on;
 public:
 	explicit	Quata_UI_Manager(QuataData&);
 	std::deque<Quata_UI_Objects*>	pass_pointers();
 
-	void		change_score(Qua::Quata_UI_Components type);
-
-	void							create_Pause();
-	void							pause_interact(sf::Vector2f mouse_pos);
-	const bool						is_mouse_hovering_in_pause() const;
-	const Qua::Button_Selection		pause_response() const;
-	void							delete_Pause();
+	void				change_score(Qua::Quata_UI_Components type);
+	void				create_Pause();
+	void				pause_interact(sf::Vector2f mouse_pos);
+	const bool			is_mouse_hovering_in_pause() const;
+	const Qua::Button_Selection	pause_response() const;
+	void				delete_Pause();
 		
 	void		create_SplashScreen(std::string);
 	void		delete_SplashScreen();
-
 };
-
-
-
 #endif
