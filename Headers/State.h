524fe9dca945fec6c3c78c5505cbecafced61e87
Braximus <utopija89@gmail.com>
2016-09-16 14:32:28 +0200

    Update and rename State.h to Headers/State.h

--- Headers/State.h
+++ Headers/State.h
@@ -10,21 +10,12 @@ class State
 {
 
 public:
-//	State() {};
 	virtual ~State() {};
 
 	virtual void			draw(sf::RenderWindow& ) = 0;
 	virtual void			update(sf::Time&) =0;
 	virtual void			handleEvents(sf::Event&) = 0;
 	virtual void			handleRealTimeInput() = 0;
-
-
-//	void					ChangeState(StateManager& state_cont, eState new_state);
-//	const eState			CheckStateID(Data& data) const;
-
-
 };
-
-
 
 #endif
