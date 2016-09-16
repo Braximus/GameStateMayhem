daf108b925de545b39966788d2266718b38f3935
Braximus <utopija89@gmail.com>
2016-09-16 14:46:20 +0200

    Update and rename Quata Pause.h to Headers/Quata Pause.h

--- Headers/Quata Pause.h
+++ Headers/Quata Pause.h
@@ -8,19 +8,16 @@ class QuataData;
 
 class Quata_Pause: public Quata_UI_Objects
 {
-	Qua::Button_Selection							mCurrentSelection, mPreviousSelection;
-	std::map<Qua::Button_Selection, Button>			mButtons;
-	sf::RectangleShape		mPause_Area, mScreen_Fill;
-	bool					hover;
+	Qua::Button_Selection				mCurrentSelection, mPreviousSelection;
+	std::map<Qua::Button_Selection, Button>		mButtons;
+	sf::RectangleShape				mPause_Area, mScreen_Fill;
+	bool						hover;
 public:
 	Quata_Pause(QuataData&);
 	void		draw(sf::RenderTarget& target, sf::RenderStates state) const;
 	void		interact(sf::Vector2f mouse_pos);
 	const bool	is_hovering() const;
 	const Qua::Button_Selection		response() const;
-
-
 };
-
 
 #endif
