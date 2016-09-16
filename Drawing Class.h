222d3d1bff1fb7a277c28bf122be1f5e2090ae55
Braximus <utopija89@gmail.com>
2016-09-16 14:51:12 +0200

    Update Drawing Class.h

--- Drawing Class.h
+++ Drawing Class.h
@@ -6,20 +6,13 @@ class Data;
 
 class Draw_Class: public sf::Drawable, public sf::Transformable
 {
-	Data&										mData;
-	typedef std::unique_ptr<Draw_Class>			Ptr;
-	std::deque<Ptr>								Drawing_container;
-
-
+	Data&						mData;
+	typedef std::unique_ptr<Draw_Class>		Ptr;
+	std::deque<Ptr>					Drawing_container;
 public:
-	explicit					Draw_Class(Data&);
+	explicit				Draw_Class(Data&);
 	virtual void				draw(sf::RenderTarget&, sf::RenderStates) const;
 	virtual void				add_ptr(Ptr*);
-
-	void						empty_container();
-
+	void					empty_container();
 };
-
-
-
 #endif
