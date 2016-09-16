c4fdfa134afdf4482160a21c3fda643e771afda7
Braximus <utopija89@gmail.com>
2016-09-16 18:33:35 +0200

    Update and rename Comet Wars SubSt Pause.h to Headers/Comet Wars SubSt Pause.h

--- Headers/Comet Wars SubSt Pause.h
+++ Headers/Comet Wars SubSt Pause.h
@@ -11,15 +11,13 @@ class CometWars_UI_Manager;
 class CometWars_SubState_Pause : public CometWarsSubStates
 {
 	CometWarsData&				mData;
-	CometWars_UI_Manager*		pUI_Manager;
+	CometWars_UI_Manager*			pUI_Manager;
 	CometWarsGame*				game;
-	bool						sound_block;
+	bool					sound_block;
 public:
 	explicit		CometWars_SubState_Pause(CometWarsData& data, CometWars_UI_Manager* ui, CometWarsGame* g);
 	void			update_Substate(sf::Time&);
 	void			handle_Events(sf::Event&);
 	void			handle_RealTimeInput();
 };
-
-
 #endif 
