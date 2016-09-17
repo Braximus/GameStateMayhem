8f88a562eb2274a90d39263ca454bddfe80aa5a3
Braximus <utopija89@gmail.com>
2016-09-17 16:35:05 +0200

    Update and rename Comet Wars Game Asteroid.cpp to CPP-Files/Comet Wars Game Asteroid.cpp

--- CPP-Files/Comet Wars Game Asteroid.cpp
+++ CPP-Files/Comet Wars Game Asteroid.cpp
@@ -11,7 +11,7 @@ CometWars_Game_Asteroid::CometWars_Game_Asteroid(CometWarsData& data, bool is_mi
 		mDamageState = CW::No_Damage;		
 
 		std::uniform_int_distribution<int>	distributor_number(0, 3);
-		UINT n = distributor_number(data.get_random_engine());		// Bira nasumicno od 0 do 3, tj. sve Major tipa asteroida.
+		UINT n = distributor_number(data.get_random_engine());		//	Chooses randomly between 0 and 3 - all Major types of comet.
 		CW::AsteroidType type = static_cast<CW::AsteroidType>(n);
 		mTextureRect = data.getComet_Texture_rect(type);
 		mAsteroid.setTextureRect(mTextureRect);
@@ -38,7 +38,7 @@ CometWars_Game_Asteroid::CometWars_Game_Asteroid(CometWarsData& data, bool is_mi
 		mDamageState = CW::Heavy_Damage;
 
 		std::uniform_int_distribution<int>	distributor_number(4, 11);
-		UINT n = distributor_number(data.get_random_engine());		// Bira nasumicno od 4 do 11, tj. sve Minor tipa asteroida..
+		UINT n = distributor_number(data.get_random_engine());		//	Chooses randomly between 4 and 11 - all Minor types of comet.
 		CW::AsteroidType type = static_cast<CW::AsteroidType>(n);
 		mTextureRect = data.getComet_Texture_rect(type);
 		mAsteroid.setTextureRect(mTextureRect);
@@ -72,9 +72,8 @@ void CometWars_Game_Asteroid::draw(sf::RenderTarget& target, sf::RenderStates st
 
 void CometWars_Game_Asteroid::update(sf::Time& time)
 {
-	//	Da bih izbegao povecanje granice asteroida tokom okretanja oko njenog centra, tokom svakog frejma uzimam trenutni angle,
-	//	resetujem angle, namestim poziciju i osvezim granice, pa tek na kraju vratim stari angle i povecam ga za standardnu velicinu;
-	
+	//	In order to avoid border increase of comets during their rotation, during every iteration I take current angle, reset it,
+	//	refresh the comet position and border and then I return the old angle and refresh it.
 	mAsteroid.move(mVelocity * time.asSeconds());
 	mPosition = mAsteroid.getPosition();
 	float old_rotation = mAsteroid.getRotation();
@@ -106,7 +105,7 @@ void CometWars_Game_Asteroid::changeDamageStatus()
 	mDamageState = static_cast<CW::AsteroidDamage>(mDamageState + 1);
 	if (mDamageState != CW::Dead)
 	{
-		//	Pretpostavlja se da je veliki asteroid.
+		//	It assumes that astroid is Major type:
 		sf::IntRect temp_rect = mAsteroid.getTextureRect();
 		mTextureRect = sf::IntRect(temp_rect.left + 50, temp_rect.top, temp_rect.width, temp_rect.height);
 		mAsteroid.setTextureRect(mTextureRect);
